86dea1c73c74e892f23b117028f4935a5163eea7
sarika gautam <sarikagautam@Sarika-GautamB114s-MacBook-Pro.local>
2021-07-13 19:54:46 +0530

    release changes

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,12 +3,12 @@ class MixedConstants {
   static const String WEBSITE_URL = 'https://atsign.com/';
 
   // for local server
-  static const String ROOT_DOMAIN = 'vip.ve.atsign.zone';
+  // static const String ROOT_DOMAIN = 'vip.ve.atsign.zone';
 
   // for staging server
   // static const String ROOT_DOMAIN = 'root.atsign.wtf';
   // for production server
-  // static const String ROOT_DOMAIN = 'root.atsign.org';
+  static const String ROOT_DOMAIN = 'root.atsign.org';
 
   static const int ROOT_PORT = 64;
 
