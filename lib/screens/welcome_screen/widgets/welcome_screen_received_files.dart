466c19f48a66d6753802812405754b1d4d4b1c21
Sachin Singh <sachins@geekyants.com>
2021-11-18 20:22:49 +0530

    received files screen data populated in home navigation bar

--- lib/screens/welcome_screen/widgets/welcome_screen_received_files.dart
+++ lib/screens/welcome_screen/widgets/welcome_screen_received_files.dart
@@ -1,4 +1,9 @@
+import 'package:atsign_atmosphere_pro/screens/common_widgets/provider_handler.dart';
+import 'package:atsign_atmosphere_pro/screens/history/widgets/received_file_list_tile.dart';
+import 'package:atsign_atmosphere_pro/services/size_config.dart';
+import 'package:atsign_atmosphere_pro/view_models/history_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WelcomeScreenReceivedFiles extends StatefulWidget {
   @override
@@ -8,14 +13,39 @@ class WelcomeScreenReceivedFiles extends StatefulWidget {
 
 class _WelcomeScreenReceivedFilesState
     extends State<WelcomeScreenReceivedFiles> {
+  HistoryProvider historyProvider;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          children: [],
-        )
-      ],
+    historyProvider = Provider.of<HistoryProvider>(context);
+    return ProviderHandler<HistoryProvider>(
+      functionName: historyProvider.RECEIVED_HISTORY,
+      showError: true,
+      successBuilder: (provider) => (provider.receivedHistoryLogs.isEmpty)
+          ? Center(
+              child: Text(
+                'No files received',
+                style: TextStyle(fontSize: 15.toFont),
+              ),
+            )
+          : ListView.separated(
+              padding: EdgeInsets.only(bottom: 170.toHeight),
+              physics: AlwaysScrollableScrollPhysics(),
+              separatorBuilder: (context, index) => Divider(indent: 16.toWidth),
+              itemCount: provider.receivedHistoryLogs.length,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ReceivedFilesListTile(
+                  key: UniqueKey(),
+                  receivedHistory: provider.receivedHistoryLogs[index],
+                  isWidgetOpen:
+                      provider.receivedHistoryLogs[index].isWidgetOpen,
+                ),
+              ),
+            ),
+      errorBuilder: (provider) => Center(
+        child: Text('Some error occured'),
+      ),
     );
   }
 }
