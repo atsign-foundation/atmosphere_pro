1140a1766dbe10831670a17360a8da64f931e869
Sachin Singh <sachins@geekyants.com>
2021-11-26 18:11:54 +0530

    file_recipients file name updated

--- lib/screens/history/widgets/file_recipients.dart
+++ lib/screens/history/widgets/file_recipients.dart
@@ -3,7 +3,6 @@ import 'package:atsign_atmosphere_pro/screens/common_widgets/person_vertical_til
 import 'package:atsign_atmosphere_pro/services/size_config.dart';
 import 'package:atsign_atmosphere_pro/utils/colors.dart';
 import 'package:atsign_atmosphere_pro/utils/text_styles.dart';
-import 'package:atsign_atmosphere_pro/view_models/file_transfer_provider.dart';
 import 'package:atsign_atmosphere_pro/view_models/history_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
