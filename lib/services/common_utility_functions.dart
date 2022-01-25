04696dce5247c1e25fe6c3679a66bbe0c6c05b56
Sachin Singh <sachins@geekyants.com>
2022-01-25 19:09:07 +0530

    delete atsign function extended for all onboarded atsigns

--- lib/services/common_utility_functions.dart
+++ lib/services/common_utility_functions.dart
@@ -1,6 +1,13 @@
 import 'dart:typed_data';
+import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:at_contact/at_contact.dart';
 import 'package:at_contacts_flutter/utils/init_contacts_service.dart';
+import 'package:atsign_atmosphere_pro/routes/route_names.dart';
+import 'package:atsign_atmosphere_pro/services/backend_service.dart';
+import 'package:atsign_atmosphere_pro/services/navigation_service.dart';
+import 'package:atsign_atmosphere_pro/utils/colors.dart';
+import 'package:atsign_atmosphere_pro/utils/text_strings.dart';
+import 'package:flutter/material.dart';
 
 class CommonUtilityFunctions {
   static final CommonUtilityFunctions _singleton =
@@ -23,5 +30,163 @@ class CommonUtilityFunctions {
     }
 
     return image;
+  }
+
+  showResetDialog() async {
+    bool isSelectAtsign = false;
+    bool isSelectAll = false;
+    var atsignsList = await KeychainUtil.getAtsignList();
+    if (atsignsList == null) {
+      atsignsList = [];
+    }
+    Map atsignMap = {};
+    for (String atsign in atsignsList) {
+      atsignMap[atsign] = false;
+    }
+    showDialog(
+        barrierDismissible: true,
+        context: NavService.navKey.currentContext,
+        builder: (BuildContext context) {
+          return StatefulBuilder(builder: (context, stateSet) {
+            return AlertDialog(
+                title: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(TextStrings.resetDescription,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 15)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Divider(
+                      thickness: 0.8,
+                    )
+                  ],
+                ),
+                content: atsignsList.isEmpty
+                    ? Column(mainAxisSize: MainAxisSize.min, children: [
+                        Text(TextStrings.noAtsignToReset,
+                            style: TextStyle(fontSize: 15)),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Close',
+                              style: TextStyle(
+                                fontSize: 15,
+                                // color: AtTheme.themecolor,
+                              ),
+                            ),
+                          ),
+                        )
+                      ])
+                    : SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            CheckboxListTile(
+                              onChanged: (value) {
+                                isSelectAll = value;
+                                atsignMap
+                                    .updateAll((key, value1) => value1 = value);
+                                // atsignMap[atsign] = value;
+                                stateSet(() {});
+                              },
+                              value: isSelectAll,
+                              checkColor: Colors.white,
+                              title: Text('Select All',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ),
+                            for (var atsign in atsignsList)
+                              CheckboxListTile(
+                                onChanged: (value) {
+                                  atsignMap[atsign] = value;
+                                  stateSet(() {});
+                                },
+                                value: atsignMap[atsign],
+                                checkColor: Colors.white,
+                                title: Text('$atsign'),
+                              ),
+                            Divider(thickness: 0.8),
+                            if (isSelectAtsign)
+                              Text(TextStrings.resetErrorText,
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 14)),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(TextStrings.resetWarningText,
+                                style: TextStyle(fontSize: 14)),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(children: [
+                              TextButton(
+                                onPressed: () async {
+                                  var tempAtsignMap = {};
+                                  tempAtsignMap.addAll(atsignMap);
+                                  tempAtsignMap.removeWhere(
+                                      (key, value) => value == false);
+                                  if (tempAtsignMap.keys.toList().isEmpty) {
+                                    isSelectAtsign = true;
+                                    stateSet(() {});
+                                  } else {
+                                    isSelectAtsign = false;
+                                    await _resetDevice(
+                                        tempAtsignMap.keys.toList());
+                                    await _onboardNextAtsign();
+                                  }
+                                },
+                                child: Text('Remove',
+                                    style: TextStyle(
+                                      color: ColorConstants.fontPrimary,
+                                      fontSize: 15,
+                                    )),
+                              ),
+                              Spacer(),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('Cancel',
+                                      style: TextStyle(
+                                          fontSize: 15, color: Colors.black)))
+                            ])
+                          ],
+                        ),
+                      ));
+          });
+        });
+  }
+
+  _resetDevice(List checkedAtsigns) async {
+    Navigator.of(NavService.navKey.currentContext).pop();
+    await BackendService.getInstance()
+        .resetAtsigns(checkedAtsigns)
+        .then((value) async {
+      print('reset done');
+    }).catchError((e) {
+      print('error in reset: $e');
+    });
+  }
+
+  _onboardNextAtsign() async {
+    var _backendService = BackendService.getInstance();
+    var atSignList = await KeychainUtil.getAtsignList();
+    if (atSignList != null &&
+        atSignList.isNotEmpty &&
+        _backendService.currentAtSign != atSignList.first) {
+      // _backendService.checkToOnboard(atSign: atSignList.first);
+      await Navigator.pushNamedAndRemoveUntil(NavService.navKey.currentContext,
+          Routes.HOME, (Route<dynamic> route) => false);
+    } else if (atSignList == null || atSignList.isEmpty) {
+      await Navigator.pushNamedAndRemoveUntil(NavService.navKey.currentContext,
+          Routes.HOME, (Route<dynamic> route) => false);
+    }
   }
 }
