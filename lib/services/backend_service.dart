83ebb656a320209f3e4590b80c00a2f2f2cdac1e
sardar1208 <sarthaksbakre@192.168.1.9>
2022-07-07 15:07:51 +0530

    fix: monitor now starts after the sync is completed

--- lib/services/backend_service.dart
+++ lib/services/backend_service.dart
@@ -277,6 +277,8 @@ class BackendService {
   }
 
   _onSuccessCallback(SyncResult syncStatus) async {
+    await startMonitor();
+
     // removes failed snackbar message.
     ScaffoldMessenger.of(NavService.navKey.currentContext!)
         .hideCurrentSnackBar();
@@ -486,7 +488,7 @@ class BackendService {
     syncWithSecondary();
 
     // start monitor and package initializations.
-    await startMonitor();
+    // await startMonitor();
     initLocalNotification();
     initializeContactsService(rootDomain: MixedConstants.ROOT_DOMAIN);
     initializeGroupService(rootDomain: MixedConstants.ROOT_DOMAIN);
