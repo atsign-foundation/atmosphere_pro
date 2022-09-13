8dc6d8fe25fcd07d20bb9da712674a36193e9e26
Sardar1208 <sarthak.bakre@gmail.com>
2022-09-13 19:01:01 +0530

    fix: scroll in delete atsign dialog

--- lib/services/common_utility_functions.dart
+++ lib/services/common_utility_functions.dart
@@ -15,6 +15,7 @@ import 'package:atsign_atmosphere_pro/utils/images.dart';
 import 'package:atsign_atmosphere_pro/utils/text_strings.dart';
 import 'package:atsign_atmosphere_pro/utils/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:showcaseview/showcaseview.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 
@@ -95,64 +96,60 @@ class CommonUtilityFunctions {
             myContext = context;
             return StatefulBuilder(builder: (context, stateSet) {
               return Dialog(
+                  child: SingleChildScrollView(
                 child: Container(
                   padding: EdgeInsets.all(20),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
+                      Row(
                         children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Showcase(
-                                  key: _one,
-                                  description:
-                                      'You can have more than one atSign associated with this app and can remove one or all of the atSigns from the app at any time.',
-                                  shapeBorder: CircleBorder(),
-                                  disableAnimation: true,
-                                  radius: BorderRadius.all(Radius.circular(40)),
-                                  showArrow: false,
-                                  overlayPadding: EdgeInsets.all(5),
-                                  blurValue: 2,
-                                  child: Text(
-                                    TextStrings.resetDescription,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                  ),
+                          Expanded(
+                            child: Showcase(
+                              key: _one,
+                              description:
+                                  'You can have more than one atSign associated with this app and can remove one or all of the atSigns from the app at any time.',
+                              shapeBorder: CircleBorder(),
+                              disableAnimation: true,
+                              radius: BorderRadius.all(Radius.circular(40)),
+                              showArrow: false,
+                              overlayPadding: EdgeInsets.all(5),
+                              blurValue: 2,
+                              child: Text(
+                                TextStrings.resetDescription,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
                                 ),
                               ),
-                              GestureDetector(
-                                onTap: () {
-                                  ShowCaseWidget.of(myContext!)
-                                      .startShowCase([_one]);
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey.shade400,
-                                      borderRadius: BorderRadius.circular(50)),
-                                  margin: EdgeInsets.all(0),
-                                  height: 20,
-                                  width: 20,
-                                  child: Icon(
-                                    Icons.question_mark,
-                                    size: 15,
-                                  ),
-                                ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              ShowCaseWidget.of(myContext!)
+                                  .startShowCase([_one]);
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade400,
+                                  borderRadius: BorderRadius.circular(50)),
+                              margin: EdgeInsets.all(0),
+                              height: 20,
+                              width: 20,
+                              child: Icon(
+                                Icons.question_mark,
+                                size: 15,
                               ),
-                            ],
+                            ),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Divider(
-                            thickness: 0.8,
-                          )
                         ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Divider(
+                        thickness: 0.8,
                       ),
                       atsignsList!.isEmpty
                           ? Column(mainAxisSize: MainAxisSize.min, children: [
@@ -178,113 +175,106 @@ class CommonUtilityFunctions {
                                 ),
                               )
                             ])
-                          : SingleChildScrollView(
-                              child: Column(
-                                children: [
+                          : Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                CheckboxListTile(
+                                  onChanged: (value) {
+                                    isSelectAll = value;
+                                    atsignMap.updateAll(
+                                        (key, value1) => value1 = value);
+                                    // atsignMap[atsign] = value;
+                                    stateSet(() {});
+                                  },
+                                  value: isSelectAll,
+                                  checkColor: Colors.white,
+                                  activeColor: Theme.of(context).primaryColor,
+                                  title: Text(TextStrings().selectAll,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                ),
+                                for (var atsign in atsignsList)
                                   CheckboxListTile(
                                     onChanged: (value) {
-                                      isSelectAll = value;
-                                      atsignMap.updateAll(
-                                          (key, value1) => value1 = value);
-                                      // atsignMap[atsign] = value;
+                                      atsignMap[atsign] = value;
                                       stateSet(() {});
                                     },
-                                    value: isSelectAll,
+                                    value: atsignMap[atsign],
                                     checkColor: Colors.white,
                                     activeColor: Theme.of(context).primaryColor,
-                                    title: Text(TextStrings().selectAll,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                        )),
+                                    title: Text('$atsign'),
                                   ),
-                                  for (var atsign in atsignsList)
-                                    CheckboxListTile(
-                                      onChanged: (value) {
-                                        atsignMap[atsign] = value;
-                                        stateSet(() {});
-                                      },
-                                      value: atsignMap[atsign],
-                                      checkColor: Colors.white,
-                                      activeColor:
-                                          Theme.of(context).primaryColor,
-                                      title: Text('$atsign'),
-                                    ),
-                                  Divider(thickness: 0.8),
-                                  if (isSelectAtsign)
-                                    Text(TextStrings.resetErrorText,
-                                        style: TextStyle(
-                                          color: Colors.red,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.normal,
-                                        )),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(TextStrings.resetWarningText,
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.normal,
-                                      )),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Row(children: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text(
-                                        TextStrings().buttonCancel,
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                      ),
-                                    ),
-                                    Spacer(),
-                                    TextButton(
-                                      style: ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStateProperty.all(
-                                                  Theme.of(context)
-                                                      .primaryColor)),
-                                      onPressed: () async {
-                                        var tempAtsignMap = {};
-                                        tempAtsignMap.addAll(atsignMap);
-                                        tempAtsignMap.removeWhere(
-                                            (key, value) => value == false);
-                                        int atsignsListLength =
-                                            tempAtsignMap.keys.toList().length;
-                                        if (tempAtsignMap.keys
-                                            .toList()
-                                            .isEmpty) {
-                                          isSelectAtsign = true;
-                                          stateSet(() {});
-                                        } else {
-                                          showConfirmationDialog(() async {
-                                            isSelectAtsign = false;
-                                            await _resetDevice(
-                                                tempAtsignMap.keys.toList());
-                                            await _onboardNextAtsign();
-                                          }, 'Remove ${atsignsListLength} atSign${atsignsListLength > 1 ? 's' : ''} from this device?');
-                                        }
-                                      },
-                                      child: Text(TextStrings().remove,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.normal,
-                                          )),
-                                    ),
-                                  ])
-                                ],
-                              ),
-                            )
+                                Divider(thickness: 0.8),
+                              ],
+                            ),
+                      if (isSelectAtsign)
+                        Text(TextStrings.resetErrorText,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            )),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(TextStrings.resetWarningText,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          )),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            TextStrings().buttonCancel,
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        TextButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  Theme.of(context).primaryColor)),
+                          onPressed: () async {
+                            var tempAtsignMap = {};
+                            tempAtsignMap.addAll(atsignMap);
+                            tempAtsignMap
+                                .removeWhere((key, value) => value == false);
+                            int atsignsListLength =
+                                tempAtsignMap.keys.toList().length;
+                            if (tempAtsignMap.keys.toList().isEmpty) {
+                              isSelectAtsign = true;
+                              stateSet(() {});
+                            } else {
+                              showConfirmationDialog(() async {
+                                isSelectAtsign = false;
+                                await _resetDevice(tempAtsignMap.keys.toList());
+                                await _onboardNextAtsign();
+                              }, 'Remove ${atsignsListLength} atSign${atsignsListLength > 1 ? 's' : ''} from this device?');
+                            }
+                          },
+                          child: Text(TextStrings().remove,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal,
+                              )),
+                        ),
+                      ])
                     ],
                   ),
                 ),
-              );
+              ));
             });
           }));
         });
