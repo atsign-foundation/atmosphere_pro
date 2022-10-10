624691be54980cabec591faf382bf948caf3c438
Ayush783 <ayushc45xxx@gmail.com>
2022-10-10 20:39:35 +0530

    added image constants

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -2,13 +2,16 @@ class ImageConstants {
   static String _basePath = 'assets/images';
 
   static String welcomeBackground = '$_basePath/welcome_bg.png';
+  static String appBackground = '$_basePath/app_bg.png';
   static String logoIcon = '$_basePath/logo.png';
   // static String myFiles = '$_basePath/myfiles.png';
   static String group = '$_basePath/group.png';
+  static String groupNew = '$_basePath/groupNew.png';
   static String trustedsenders = '$_basePath/trustedsenders.png';
   static String drawerIcon = '$_basePath/menu.png';
   static String contactsIcon = '$_basePath/contacts.png';
   static String blockedIcon = '$_basePath/blocked.png';
+  static String blockedIconNew = '$_basePath/blockedNew.png';
   static String faqsIcon = '$_basePath/faqs.png';
   static String termsAndConditionsIcon = '$_basePath/termsAndConditions.png';
   static String transferHistoryIcon = '$_basePath/transferHistory.png';
@@ -37,11 +40,19 @@ class ImageConstants {
   static String exclamation = '$_basePath/exclamation.png';
   static String close = '$_basePath/close.png';
   static String trustedSender = '$_basePath/trustedSendersIcon.png';
+  static String trustedSenderNew = '$_basePath/trustedSender.png';
   static String myFiles = '$_basePath/my_files.png';
   static String groups = '$_basePath/groups.png';
   static String contactUs = '$_basePath/contactUs.png';
+  static String searchIcon = '$_basePath/search.png';
+  static String refreshIcon = '$_basePath/refresh.png';
+  static String arrowDown = '$_basePath/arrow_down.png';
   // TEST FILE DELETE WHEN IN PRODUCTION
   static String test = '$_basePath/test.jpg';
+  static String pptLogo = '$_basePath/ppt_logo.png';
+  static String psdLogo = '$_basePath/psd_logo.png';
+  static String htmlLogo = '$_basePath/html_logo.png';
+  static String pngLogo = '$_basePath/png_logo.png';
 
   // Sample profile pictures
   static String barbara = '$_basePath/barbara.jpg';
