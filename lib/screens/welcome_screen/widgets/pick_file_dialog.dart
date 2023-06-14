769d4e211c882168ff901be4a1126ae83ea2c96d
Sarthak Suresh Bakre <sarthaksureshbakre@Sarthaks-MacBook-Pro.local>
2023-06-14 13:39:10 +0530

    feat: added dialog box for file type choice

---
+++ lib/screens/welcome_screen/widgets/pick_file_dialog.dart
@@ -0,0 +1,61 @@
+import 'package:atsign_atmosphere_pro/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../utils/text_strings.dart';
+import '../../../utils/text_styles.dart';
+
+class PickFileDialog extends StatelessWidget {
+  const PickFileDialog({Key? key, required this.selectFiles}) : super(key: key);
+
+  final Function selectFiles;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          InkWell(
+            onTap: () {
+              selectFiles("MEDIA");
+            },
+            child: Row(children: <Widget>[
+              Icon(
+                Icons.camera,
+                size: 30,
+                color: ColorConstants.redText,
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    TextStrings().choice1,
+                    style: CustomTextStyles.primaryBold14,
+                  ))
+            ]),
+          ),
+          const SizedBox(height: 15),
+          InkWell(
+            onTap: () {
+              selectFiles("FILES");
+            },
+            child: Row(children: <Widget>[
+              Icon(
+                Icons.file_copy,
+                size: 30,
+                color: ColorConstants.redText,
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    TextStrings().choice2,
+                    style: CustomTextStyles.primaryBold14,
+                  ))
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen/widgets/welcome_sceen_home.dart
+++ lib/screens/welcome_screen/widgets/welcome_sceen_home.dart
@@ -8,6 +8,7 @@ import 'package:atsign_atmosphere_pro/screens/common_widgets/file_card.dart';
 import 'package:atsign_atmosphere_pro/screens/common_widgets/provider_callback.dart';
 import 'package:atsign_atmosphere_pro/screens/common_widgets/switch_at_sign.dart';
 import 'package:atsign_atmosphere_pro/screens/welcome_screen/widgets/overlapping_contacts.dart';
+import 'package:atsign_atmosphere_pro/screens/welcome_screen/widgets/pick_file_dialog.dart';
 import 'package:atsign_atmosphere_pro/services/backend_service.dart';
 import 'package:at_common_flutter/services/size_config.dart';
 import 'package:atsign_atmosphere_pro/services/navigation_service.dart';
@@ -24,6 +25,7 @@ import 'package:open_file/open_file.dart';
 import 'package:provider/provider.dart';
 
 import '../../../utils/images.dart';
+import '../../../utils/text_styles.dart';
 import '../../common_widgets/app_bar_custom.dart';
 import 'choice_contacts_widget.dart';
 
@@ -131,7 +133,14 @@ class _WelcomeScreenHomeState extends State<WelcomeScreenHome> {
                     );
                   } else {
                     return InkWell(
-                      onTap: selectFiles,
+                      onTap: () async {
+                        await showDialog(
+                          context: context,
+                          builder: (context) {
+                            return PickFileDialog(selectFiles: selectFiles,);
+                          },
+                        );
+                      },
                       child: Container(
                         height: 142.toHeight,
                         width: double.infinity,
@@ -312,7 +321,7 @@ class _WelcomeScreenHomeState extends State<WelcomeScreenHome> {
 
   Widget _buildAddFilesOption() {
     return InkWell(
-      onTap: selectFiles,
+      onTap: selectFiles("FILES"),
       child: Container(
         height: 61.toHeight,
         width: double.infinity,
@@ -369,9 +378,9 @@ class _WelcomeScreenHomeState extends State<WelcomeScreenHome> {
     }
   }
 
-  selectFiles() async {
+  selectFiles(String choice) async {
     await providerCallback<FileTransferProvider>(context,
-        task: (provider) => provider.pickFiles(provider.FILES),
+        task: (provider) => provider.pickFiles(choice),
         taskName: (provider) => provider.PICK_FILES,
         onSuccess: (provider) {},
         onError: (err) => ErrorDialog().show(err.toString(), context: context));
