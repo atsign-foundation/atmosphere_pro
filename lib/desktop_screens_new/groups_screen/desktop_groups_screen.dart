04b3056b5aaea619843e1deef803529e8153776d
sonle <sonle@geekyants.com>
2023-08-25 13:53:09 +0700

    fix: add transfer group method and fix empty group screen

--- lib/desktop_screens_new/groups_screen/desktop_groups_screen.dart
+++ lib/desktop_screens_new/groups_screen/desktop_groups_screen.dart
@@ -29,9 +29,11 @@ class _DesktopGroupsScreenState extends State<DesktopGroupsScreen> {
   List<AtGroup>? previousData;
 
   AtSignLogger atSignLogger = AtSignLogger('DesktopGroupInitialScreen');
+  late DesktopGroupsScreenProvider groupsProvider;
 
   @override
   void initState() {
+    groupsProvider = context.read<DesktopGroupsScreenProvider>();
     try {
       super.initState();
       GroupService().groupPreferece.showBackButton = widget.showBackButton;
@@ -43,117 +45,68 @@ class _DesktopGroupsScreenState extends State<DesktopGroupsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFF8F8F8),
-      child: StreamBuilder(
-        stream: GroupService().atGroupStream,
-        builder: (BuildContext context, AsyncSnapshot<List<AtGroup>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (snapshot.hasError) {
-              return ErrorScreen(onPressed: () {
-                GroupService().getAllGroupsDetails();
-              });
-            } else {
-              if (snapshot.hasData) {
-                if ((previousData == null) ||
-                    (!areListsEqual(previousData, snapshot.data))) {
-                  shouldUpdate = true;
-                  previousData = snapshot.data;
-                } else {
-                  shouldUpdate = false;
-                }
-
-                if (snapshot.data!.isEmpty) {
-                  return createBtnTapped
-                      ? NestedNavigators(
-                          snapshot.data!,
-                          () {
-                            setState(() {
-                              createBtnTapped = false;
-                            });
-                          },
-                          shouldUpdate: shouldUpdate,
-                          key: UniqueKey(),
-                          expandIndex: 0,
-                        )
-                      : DesktopEmptyGroup(createBtnTapped, onCreateBtnTap: () {
-                          setState(() {
-                            createBtnTapped = true;
-                          });
-                        });
-                } else {
-                  return NestedNavigators(
-                    snapshot.data!,
-                    () {
-                      setState(() {
-                        createBtnTapped = false;
-                      });
-                    },
-                    shouldUpdate: shouldUpdate,
-                    key: UniqueKey(),
-                    expandIndex: GroupService().expandIndex ?? 0,
-                  );
-                }
-              } else {
-                return DesktopEmptyGroup(createBtnTapped, onCreateBtnTap: () {
-                  setState(() {
-                    createBtnTapped = true;
-                  });
-                });
-              }
-            }
-          }
-        },
-      ),
-    );
-  }
-}
-
-class NestedNavigators extends StatefulWidget {
-  final List<AtGroup> data;
-  final Function initialRouteOnArrowBackTap;
-  final bool shouldUpdate;
-  final int expandIndex;
-
-  const NestedNavigators(this.data, this.initialRouteOnArrowBackTap,
-      {Key? key, this.shouldUpdate = false, required this.expandIndex})
-      : super(key: key);
-
-  @override
-  _NestedNavigatorsState createState() => _NestedNavigatorsState();
-}
-
-class _NestedNavigatorsState extends State<NestedNavigators> {
-  late DesktopGroupsScreenProvider groupsProvider;
-
-  @override
-  void initState() {
-    groupsProvider = context.read<DesktopGroupsScreenProvider>();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Consumer<DesktopGroupsScreenProvider>(
-      builder: (BuildContext context, provider, Widget? child) {
-        return Stack(
+        builder: (context, provider, child) {
+      return Container(
+        color: const Color(0xFFF8F8F8),
+        child: Stack(
           children: [
-            widget.data.isEmpty
-                ? const DesktopEmptyGroup(true)
-                : DesktopGroupsList(
-                    widget.data,
-                    key: UniqueKey(),
-                    expandIndex: widget.expandIndex,
-                    onExpand: (value) {
-                      provider.setSelectedAtGroup(value);
-                      provider.setGroupCardState(GroupCardState.expanded);
-                    },
-                    onAdd: () {
-                      provider.setGroupCardState(GroupCardState.add);
-                    },
-                  ),
+            StreamBuilder(
+              stream: GroupService().atGroupStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<AtGroup>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  if (snapshot.hasError) {
+                    return ErrorScreen(onPressed: () {
+                      GroupService().getAllGroupsDetails();
+                    });
+                  } else {
+                    if (snapshot.hasData) {
+                      if ((previousData == null) ||
+                          (!areListsEqual(previousData, snapshot.data))) {
+                        shouldUpdate = true;
+                        previousData = snapshot.data;
+                      } else {
+                        shouldUpdate = false;
+                      }
+
+                      if (snapshot.data!.isEmpty) {
+                        return Center(
+                          child: DesktopEmptyGroup(
+                              provider.groupCardState == GroupCardState.add,
+                              onCreateBtnTap: () {
+                            provider.setGroupCardState(GroupCardState.add);
+                          }),
+                        );
+                      } else {
+                        return DesktopGroupsList(
+                          snapshot.data!,
+                          key: UniqueKey(),
+                          expandIndex: GroupService().expandIndex ?? 0,
+                          onExpand: (value) {
+                            provider.setSelectedAtGroup(value);
+                            provider.setGroupCardState(GroupCardState.expanded);
+                          },
+                          onAdd: () {
+                            provider.setGroupCardState(GroupCardState.add);
+                          },
+                        );
+                      }
+                    } else {
+                      return Center(
+                        child: DesktopEmptyGroup(
+                            provider.groupCardState == GroupCardState.add,
+                            onCreateBtnTap: () {
+                          provider.setGroupCardState(GroupCardState.add);
+                        }),
+                      );
+                    }
+                  }
+                }
+              },
+            ),
             if (provider.groupCardState != GroupCardState.disable)
               Row(
                 children: [
@@ -168,11 +121,11 @@ class _NestedNavigatorsState extends State<NestedNavigators> {
                     child: buildGroupCard(provider.groupCardState),
                   ),
                 ],
-              )
+              ),
           ],
-        );
-      },
-    );
+        ),
+      );
+    });
   }
 
   Widget buildGroupCard(GroupCardState state) {
