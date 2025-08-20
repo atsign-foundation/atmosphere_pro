f3e4b1dcbce21ee914e7e1ef0c621b09aa19dee7
Curtly Critchlow <curtlycritchlow@ccwc.io>
2025-08-20 09:10:03 -0400

    fix: update activeColor to activeThumbColor for Switch widget in settings_desktop.dart

--- lib/desktop_screens_new/settings_screen/settings_desktop.dart
+++ lib/desktop_screens_new/settings_screen/settings_desktop.dart
@@ -192,7 +192,7 @@ class _SettingsScreenDesktopState extends State<SettingsScreenDesktop> {
                             ),
                             SizedBox(width: 16),
                             Switch(
-                                activeColor: ColorConstants.orange,
+                                activeThumbColor: ColorConstants.orange,
                                 value: enableShareStSign,
                                 onChanged: (check) async {
                                   if (check) {
